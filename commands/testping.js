e0cf5c6025a91adca7f134cd278443330368c4b5
tecdude <fab_tec@icloud.com>
2022-11-27 17:05:23 +0000

    ping gives value

--- commands/testping.js
+++ commands/testping.js
@@ -1,17 +1,13 @@
-const { SlashCommandBuilder, EmbedBuilder, InteractionResponse, WebSocketManager } = require('discord.js');
-
-
-const pingEmbed = new EmbedBuilder()
-	.setColor(0x20c1ed)
-	.setTitle('Hello!')
-	.setDescription(`Pong :ping_pong:`)
-	.setTimestamp()
+const { SlashCommandBuilder, EmbedBuilder, WebSocketManager, Client } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Check if the bot is online'),
 	async execute(interaction) {
-		await interaction.reply({ embeds: [pingEmbed] });
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		sent
+		interaction.editReply(`Pong! :ping_pong:\n**Roundtrip latency:** ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		//await interaction.reply({ embeds: [pingEmbed] });
 	},
 };
