bfdaa4a44987728b7a1acca9c8bfaaadc7a4d21a
tecdude <fab_tec@icloud.com>
2022-11-27 16:04:10 +0000

    status stuff

--- commands/testping.js
+++ commands/testping.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder, InteractionResponse } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, InteractionResponse, WebSocketManager } = require('discord.js');
+
 
 const pingEmbed = new EmbedBuilder()
 	.setColor(0x20c1ed)
-	.setTitle('Pong')
-	.setDescription(`:ping_pong:`)
+	.setTitle('Hello!')
+	.setDescription(`Pong :ping_pong:`)
 	.setTimestamp()
 
 module.exports = {
---
+++ status.js
@@ -0,0 +1,9 @@
+const { Client, IntentsBitField, PresenceUpdateStatus } = require('discord.js');
+
+//const myIntents = new IntentsBitField();
+//myIntents.add(IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.GuildMembers);
+
+//const client = new Client({ intents: myIntents });
+
+//client.user.setPresence({ activities: [{ name: `the Swift Den`,  type: `WATCHING` }] });
+PresenceUpdateStatus.Online
