6174615e17b22db4e7e2d6926c3a063056cb3efe
tecdude <fab_tec@icloud.com>
2022-11-27 11:07:25 +0000

    fix timestamp

--- commands/info.js
+++ commands/info.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
+const timestamp = Math.floor(Date.now() / 1000)
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('Server info.'),
 	async execute(interaction) {
-        let cdate = new Date;
-        cdate = cdate.getTime() + cdate.getDate();
-		await interaction.reply(`**${interaction.guild.name}\n**> Date: ${cdate}\n > Member count: ${interaction.guild.memberCount}`);
+//        let cdate = new Date;
+//        cdate = cdate.getDate();
+		await interaction.reply(`**${interaction.guild.name}**\n> Date: <t:${timestamp}>\n > Member count: ${interaction.guild.memberCount}`);
 	},
 };
