a68eb75d6421f0a3666e110a8b5c2564fe3770cb
tecdude <92227828+tecdude@users.noreply.github.com>
2022-12-12 17:39:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.login(token);
 
 client.on(Events.ClientReady, () => {
     console.warn("Bot is online");
-    client.user.setActivity('/help for more info')
+    client.user.setActivity('/help')
 });
 
 
