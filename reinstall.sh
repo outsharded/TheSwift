9a66d5b56157aee3368a12368ac9f885c8a77c6f
tectrainguy <fab_tec@icloud.com>
2023-03-14 18:56:32 +0000

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,7 +3,7 @@ cd
 pm2 stop Swift
 mv TheSwift/config.json config.json
 rm -r TheSwift
-git clone https://github.com/tecdude/TheSwift.git
+git clone https://github.com/tectrainguy/TheSwift.git
 mv config.json TheSwift/config.json
 cd TheSwift
 npm install
