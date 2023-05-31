bcfb7c73fa651e324598b6440f11c7559da270a5
tectrainguy <fab_tec@icloud.com>
2023-05-31 14:36:59 +0100

    solid sharding
    fixed at 2 shards

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { token } = require("./config.json");
 
 const manager = new ShardingManager('./bot_deploy.js', { token: token });
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.on('shardCreate', shard => shard.on("ready", () => {
+    console.log(`Shard ${shard.id} connected.`)
+    // Sending the data to the shard.
+    shard.send({type: "shardId", data: {shardId: shard.id}});
+}))
 
-manager.spawn();
+manager.spawn({amount: 2, delay: 10000});
