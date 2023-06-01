29b912c306d490a15b205151870af15d31903bc2
tectrainguy <fab_tec@icloud.com>
2023-06-01 09:04:07 +0100

    moved sharding to settings for simplicity

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ShardingManager } = require('discord.js');
-const { token, shards } = require("./config.json");
+const { token } = require("./config.json");
+const { shards } = require("./settings.json")
 
 const manager = new ShardingManager('./bot_deploy.js', { token: token });
 
